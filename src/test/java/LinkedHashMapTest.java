b7c1bfb72adbb43dc6fa123e5df03142c4b349b0
krishnadontaraju <reachkrishnadontaraju@gmail.com>
2021-04-14 00:05:43 +0530

    ADDED : validated hash map(hash table) with a test

---
+++ src/test/java/LinkedHashMapTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class LinkedHashMapTest {
+    @Test
+    public void givenASentence_whenWordsAreAddedToList_ShouldReturnParanoidFrequency(){
+        String sentence = "Paranoids are not paranoid because they are paranoid but because " +
+                "they keep putting themselves deliberately into paranoid avoidable situations";
+
+        LinkedHashMap<String , Integer > customLinkedHashMap = new LinkedHashMap<>();
+        String[] sentenceToWords = sentence.toLowerCase().split(" ");
+        for (String word : sentenceToWords) {
+            Integer value = customLinkedHashMap.get(word);
+            if (value == null)
+                value = 1;
+            else
+                value++;
+            customLinkedHashMap.add(word,value);
+        }
+        int frequency = customLinkedHashMap.get("paranoid");
+        Assertions.assertEquals(3,frequency);
+    }
+}
