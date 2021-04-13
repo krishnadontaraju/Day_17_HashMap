b9485df0e68834a6ce0c1b67d4f21f5c34ce9d59
krishnadontaraju <reachkrishnadontaraju@gmail.com>
2021-04-13 22:38:25 +0530

    Welcome To Hash Map Data Structure

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("THIS IS THE LINKED LIST OPERATION TEST, WELCOME");
+        System.out.println("THIS HASH MAP DATA STRUCTURE, WELCOME");
+
     }
 }
